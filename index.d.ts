6d17e57baa51eba82eda2e06fddd2866712189bb
root <root@localhost>
2021-09-10 21:34:57 +0200

    add .d.ts

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+export declare interface Options {
+  cwd?: string = '.';
+  pattern?: string = '**/*.md';
+  incremental?: boolean = false;
+  fstat?: boolean = false;
+}
+
+export declare interface Data {
+  header: {
+    cwd: string;
+    path: string;
+    dirname: string;
+    basename: string;
+    extname: string;
+  } & Partial<fs.Stats>,
+  attr: { [key: string]: unknown },
+  body: string;
+}
+
+declare const _default: (options: Options) => Iterable<Data>;
+export default _default;
